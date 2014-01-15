34efb77fb97d7402345aee035eb51560eaed2504
jacobpurcell <jacob.purcell@grantadesign.com>
2014-01-15 15:58:13 +0000

    create expenses controller, and example of returning JSON

--- rest-api/Controllers/ExpensesController.cs
+++ rest-api/Controllers/ExpensesController.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Helpers;
 using System.Web.Http;
+using MongoDB.Bson;
 
 namespace Expenses.Controllers
 {
-    public class ValuesController : ApiController
+    public class ExpensesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public HttpResponseMessage Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+
+            var data = new []
+            {
+                new {FieldA = "1", FieldB = "2"},
+                new {FieldA = "10", FieldB = "20"}
+            };
+
+            return Request.CreateResponse(HttpStatusCode.OK, data, new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         // GET api/values/5
