a2d9cf14a9240420397bc98f89ac6f46c69d67ec
jennifercampbell <jennifer.campbell@grantadesign.com>
2014-01-16 16:06:52 +0000

    Get Categories from a repository
    Currently implemented as a flat file

--- rest-api/Controllers/CategoryController.cs
+++ rest-api/Controllers/CategoryController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using Expenses.Models;
+using Newtonsoft.Json;
 
 namespace Expenses.Controllers
 {
@@ -14,11 +19,9 @@ namespace Expenses.Controllers
         // GET api/category
         public HttpResponseMessage Get()
         {
-            var categories = new object[]
-                {
-                    new { Name="Train", Details = new { OutDate="01/01/1900", From="Startpoint", To="Destination"} },
-                    new { Name="Car Rental", Details = new { NumberOfDays=10 } }
-                };
+            var repository = new Repository();
+            var categories = repository.GetCategories();
+
             return Request.CreateResponse(HttpStatusCode.OK, categories, JsonMediaType);
         }
 
@@ -32,6 +35,15 @@ namespace Expenses.Controllers
         // POST api/category
         public void Post([FromBody]string value)
         {
+            // TODO: get the categories from the posted data
+            var categories = new object[]
+            {
+                new { Name="Train", Details = new { OutDate="01/01/1900", From="Startpoint", To="Destination"} },
+                new { Name="Car Rental", Details = new { NumberOfDays=10 } }
+            };
+
+            var repository = new Repository();
+            repository.SaveCategories(categories);
         }
 
         // PUT api/category/5
---
+++ rest-api/Models/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Expenses.Models
+{
+    public class Repository
+    {
+        public IEnumerable GetCategories()
+        {
+            ArrayList categories = new ArrayList();
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(HttpContext.Current.Server.MapPath("~/App_Data/categories.txt"));
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                categories.Add(JsonConvert.DeserializeObject(line));
+            }
+
+            file.Close();
+
+            return categories;
+        }
+
+        public void SaveCategories(object[] categories)
+        {
+            // Write the string to a file.
+            System.IO.StreamWriter file = new System.IO.StreamWriter(HttpContext.Current.Server.MapPath("~/App_Data/categories.txt"));
+            foreach (var category in categories)
+            {
+                var line = JsonConvert.SerializeObject(category);
+                file.WriteLine(line);
+            }
+            
+            file.Close();
+        }
+    }
+}
