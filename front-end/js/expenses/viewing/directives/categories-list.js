b4f1dd3b21c80b313bfad8839f4ae999c55c18e7
jennifercampbell <jennifer.campbell@grantadesign.com>
2014-01-16 17:37:38 +0000

    Create a new category stage 2: move the directive out into its own file

---
+++ front-end/js/expenses/viewing/directives/categories-list.js
@@ -0,0 +1,24 @@
+﻿app.factory('apiUrl', function () {
+    return "/ExpensesApi";
+});
+
+app.factory('categoriesList', ['$q', '$resource', 'apiUrl', function ($q, $resource, apiUrl) {
+    var categoriesResource = $resource(apiUrl + '/api/category');
+    var categories = categoriesResource.query(function () {
+    }, true);
+
+    return categories;
+}]);
+
+app.directive('categoriesList', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/expenses/viewing/templates/categories-list.html',
+        controller: function ($scope, categoriesList) {
+            $scope.categories = categoriesList;
+            $scope.addCategory = function () {
+                $scope.categories.push($scope.newCategory);
+            };
+        }
+    };
+});
