3cbb5e1cb869a45e9465a3032fc50358674a820c
jacobpurcell <jacob.purcell@grantadesign.com>
2014-01-17 11:02:30 +0000

    expense-claim-form: generate correct input types based on field ...
    ... also includes validation.
    
    Supported fields are string, date, and decimal

--- front-end/js/expenses/creation/directives/expense-form.js
+++ front-end/js/expenses/creation/directives/expense-form.js
@@ -1,7 +1,7 @@
 ﻿app.factory('commonFields', [function () {
     return [
-        { name: "Project", type: 'String' },
-        { name: "Date", type: 'Date' },
+        { name: "Project", type: 'String', required: true },
+        { name: "Date", type: 'Date', required: true },
         { name: "Amount", type: "Decimal" },
         { name: "Currency", type: "String" },
         { name: "Rate", type: "Decimal" },
@@ -14,14 +14,16 @@ app.directive('expenseForm', [function () {
         restrict: 'E',
         templateUrl: 'js/expenses/creation/templates/expense-form.html',
         controller: function ($scope, commonFields) {
-            $scope.fields = _(commonFields).map(function(field) {
+            $scope.fields = _(commonFields).map(function (field) {
                 return {
                     name: field.name,
+                    type: field.type,
                     value: '',
+                    required: field.required
                 };
             });
 
-            $scope.createExpense = function() {
+            $scope.createExpense = function () {
                 
             };
         }
