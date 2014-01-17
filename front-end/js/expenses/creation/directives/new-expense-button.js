85f13c6608afbfef389c4e782f33053fb434e79b
jacobpurcell <jacob.purcell@grantadesign.com>
2014-01-17 12:37:15 +0000

    button for new-expense dialog

---
+++ front-end/js/expenses/creation/directives/new-expense-button.js
@@ -0,0 +1,26 @@
+﻿app.directive('newExpenseButton', function() {
+
+    return {
+        restrict: 'E',
+        template: "<button ng-click='open()'>Add Expense</button>",
+        controller: function($scope, $modal) {
+            $scope.open = function() {
+                $modal.open({
+                    templateUrl: 'js/expenses/creation/templates/add-expense-dialog.html',
+
+                    controller: function($scope, $modalInstance) {
+                        $scope.ok = function() {
+                            $modalInstance.close();
+                        };
+
+                        $scope.cancel = function() {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    },
+                });
+            };
+
+        }
+    };
+
+});
